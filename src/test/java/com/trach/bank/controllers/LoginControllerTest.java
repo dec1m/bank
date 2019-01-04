b7a4ec91ac6634f6c9bd66d91f86006b47c75ecb
Eugen Trach <eugen.trach@gmail.com>
2019-01-04 20:26:31 +0200

    Add tests for LoginController

---
+++ src/test/java/com/trach/bank/controllers/LoginControllerTest.java
@@ -0,0 +1,45 @@
+package com.trach.bank.controllers;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+import static org.mockito.Mockito.*;
+import static junit.framework.TestCase.assertEquals;
+
+public class LoginControllerTest {
+
+    private LoginController controller;
+    private ModelAndView model;
+    @Before
+    public void setUp(){
+        controller = new LoginController();
+        model = mock(ModelAndView.class);
+
+    }
+
+    @Test
+    public void login_value_in_model_Test(){
+        ModelAndView modelAndView = controller.login();
+        assertEquals("/login",modelAndView.getViewName());
+
+    }
+
+    @Test
+    public void login_return_value_Test(){
+        ModelAndView expected = new ModelAndView();
+        expected.setViewName("/login");
+        ModelAndView actual = controller.login();
+        assertEquals(actual.getViewName(),expected.getViewName());
+        assertEquals(expected.getClass(),actual.getClass());
+
+
+    }
+    @Test
+    public void access_return_value_Test(){
+        String expected = "/accessDenied";
+        String actual = controller.access();
+        assertEquals(expected,actual);
+    }
+
+}
